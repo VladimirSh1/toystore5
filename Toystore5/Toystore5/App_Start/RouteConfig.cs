b84abbc69d9f73c7ca15dc8f483dfdb1bd9ade04
VladimirSh1 <v.shimansky@mail.ru>
2020-11-19 21:52:21 +0200

    second commit

--- Toystore5/Toystore5/App_Start/RouteConfig.cs
+++ Toystore5/Toystore5/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Toystore5
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Toys", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Toystore5/Toystore5/Controllers/ToysController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Toystore5.Models;
+
+namespace Toystore5.Controllers
+{
+    public class ToysController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Toys
+        public ActionResult Index()
+        {
+            TempData["ReturnUrl"] = "Toys/Index/";
+            return View(db.Toys.ToList());
+        }
+
+        // GET: Toys/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Toy toy = db.Toys.Find(id);
+            if (toy == null)
+            {
+                return HttpNotFound();
+            }
+            return View(toy);
+        }
+
+        // GET: Toys/Create
+        [Authorize(Roles = "admin")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Toys/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Price,Name")] Toy toy)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Toys.Add(toy);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(toy);
+        }
+
+        // GET: Toys/Edit/5
+        [Authorize(Roles = "admin")]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Toy toy = db.Toys.Find(id);
+            if (toy == null)
+            {
+                return HttpNotFound();
+            }
+            return View(toy);
+        }
+
+        // POST: Toys/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Price,Name")] Toy toy)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(toy).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(toy);
+        }
+
+        // GET: Toys/Delete/5
+        [Authorize(Roles ="admin")]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Toy toy = db.Toys.Find(id);
+            if (toy == null)
+            {
+                return HttpNotFound();
+            }
+            return View(toy);
+        }
+
+        // POST: Toys/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Toy toy = db.Toys.Find(id);
+            db.Toys.Remove(toy);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
